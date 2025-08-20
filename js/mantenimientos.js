abb16dcf3171cddd3d26ef038b1e783902a5703d
kkunes <lazona0690@gmail.com>
2025-08-20 14:12:15 -0600

    Add files via upload

--- js/mantenimientos.js
+++ js/mantenimientos.js
@@ -126,6 +126,12 @@ export async function mostrarMantenimientos() {
         `;
 
         contenedor.innerHTML = `
+            <div class="flex items-center mb-6">
+                <svg class="w-8 h-8 text-gray-700 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z"></path>
+                </svg>
+                <h2 class="text-2xl font-bold text-gray-800">Gestión de Mantenimientos</h2>
+            </div>
             <div id="total-mantenimientos-container" class="mb-6"></div>
             ${filtrosHtml}
             ${btnNuevoHtml}
